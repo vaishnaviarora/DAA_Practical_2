9da24e37613288b1ba4bbd4ba9645bbe0a81c08d
vaishnaviarora <123967735+vaishnaviarora@users.noreply.github.com>
2023-01-30 22:57:00 +0530

    Add files via upload

---
+++ q_dequeue.java
@@ -0,0 +1,65 @@
+class Queue {
+	int front, rear, size;
+	int capacity;
+	int array[];
+
+	public Queue(int capacity)
+	{
+		this.capacity = capacity;
+		front = this.size = 0;
+		rear = capacity - 1;
+		array = new int[this.capacity];
+	}
+
+	boolean isFull(Queue queue)
+	{
+		return (queue.size == queue.capacity);
+	}
+
+	boolean isEmpty(Queue queue)
+	{
+		return (queue.size == 0);
+	}
+
+	void enqueue(int item)
+	{
+		if (isFull(this))
+			return;
+		this.rear = (this.rear + 1)
+					% this.capacity;
+		this.array[this.rear] = item;
+		this.size = this.size + 1;
+		System.out.println(item
+						+ " enqueued to queue");
+	}
+
+	int dequeue()
+	{
+		if (isEmpty(this))
+			return -1;
+
+		int item = this.array[this.front];
+		this.front = (this.front + 1)
+					% this.capacity;
+		this.size = this.size - 1;
+		return item;
+	}
+}
+
+public class q_dequeue {
+	public static void main(String[] args)
+	{
+		Queue queue = new Queue(1000);
+
+		queue.enqueue(10);
+		queue.enqueue(20);
+		queue.enqueue(30);
+		queue.enqueue(40);
+
+		System.out.println(queue.dequeue()
+						+ " dequeued from queue");
+        System.out.println(queue.dequeue()
+						+ " dequeued from queue");
+	}
+}
+
